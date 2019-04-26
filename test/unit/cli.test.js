8dae6079bc93675db14e271aaf3ce7c9997c0ea6
Ahmetcan ÖZCAN <ahmetcanozcan7@gmail.com>
2019-04-26 14:18:10 +0300

    Fixed test bugs

--- test/unit/cli.test.js
+++ test/unit/cli.test.js
@@ -1,11 +1,19 @@
 var cli = require('../../lib/cli');
-
+const _ = require('lodash');
 test('Parses command correctly', () => {
 
 
   expect(cli.parse('node glevn . --src=bin/.env'.split(' ')).options).toMatchObject({
     'src': 'bin/.env'
   });
+
+
+
+  expect(_.pick(cli.parse('node glevn --no-env --env-src .envfile --set-globals').options, ["env", "envSrc", "setGlobals"])).toMatchObject({
+    env: false,
+    envSrc: ".envfile",
+    setGlobals: true
+  })
 
   expect(cli.parse('node glevn app.js --src bin/.env').execFile).toMatch(/.js/);
 
